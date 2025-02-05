3ec319c45dee1d34fb4a0d23e6df5bd7baf19cb5
loedn <loedn@pm.me>
2025-02-05 11:02:32 +0100

    added some pattern matching on requests

--- lib/anton/handler.ex
+++ lib/anton/handler.ex
@@ -2,31 +2,48 @@ defmodule Anton.Handler do
   def handle(request) do
     request
     |> parse
+    |> log
     |> route
     |> format_response
   end
 
+  def log(conv) do
+    IO.inspect conv
+  end
+
   def parse(request) do
-    # TODO: parse request string into map
-    conv = %{ method: "GET", path: "/wildthings", resp_body: ""}
+    [method, path, _] =
+      request
+      |> String.split("\n")
+      |> List.first
+      |> String.split(" ")
+
+      %{ method: method, path: path, resp_body: ""}
   end
 
   def route(conv) do
-    # TODO: create map that has response body
-    conv = %{ method: "GET", path: "wildthings", resp_body: "Bears, Lions, Tigers"}
+    route(conv, conv.method, conv.path)
+  end
+
+  def route(conv, "GET", "/wildthings") do
+    %{ conv | resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def route(conv, "GET", "/bears") do
+    %{ conv | resp_body: "Winnie, paddington, smokey"}
   end
 
   def format_response(conv) do
-  # TODO: use vals in map to create http response string
   """
   HTTP/1.1 200 OK
   Content-Type: text/html
-  Content-Length: 20
+  Content-Length: #{String.length(conv.resp_body)}
 
-  Bears, Lions, Tigers
+  #{conv.resp_body}
   """
   end
 end
+
 request = """
 GET /wildthings HTTP/1.1
 Host: example.com
@@ -35,5 +52,28 @@ Accept:*/*
 
 """
 
-response = Anton.handler(request)
+response = Anton.Handler.handle(request)
+IO.puts response
+
+
+request = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept:*/*
+
+"""
+
+response = Anton.Handler.handle(request)
+IO.puts response
+
+request = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept:*/*
+
+"""
+
+response = Anton.Handler.handle(request)
 IO.puts response
