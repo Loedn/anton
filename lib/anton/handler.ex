c720a51b43b4974a73a5bd44d573be703ab0c90f
loedn <loedn@pm.me>
2025-02-05 11:16:51 +0100

    pattern matching

--- lib/anton/handler.ex
+++ lib/anton/handler.ex
@@ -18,7 +18,11 @@ defmodule Anton.Handler do
       |> List.first
       |> String.split(" ")
 
-      %{ method: method, path: path, resp_body: ""}
+      %{ method: method,
+        path: path,
+        resp_body: "",
+        status: nil
+      }
   end
 
   def route(conv) do
@@ -26,21 +30,41 @@ defmodule Anton.Handler do
   end
 
   def route(conv, "GET", "/wildthings") do
-    %{ conv | resp_body: "Bears, Lions, Tigers"}
+    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
   def route(conv, "GET", "/bears") do
-    %{ conv | resp_body: "Winnie, paddington, smokey"}
+    %{ conv | status: 200, resp_body: "Winnie, paddington, smokey"}
+  end
+
+  def route(conv, "GET", "/bears/" <> id) do
+    %{ conv | status: 200, resp_body: "Bear #{id}"}
+  end
+
+  # catchall function needs to be last
+  def route(conv, _method, path) do
+    %{ conv | status: 404, resp_body: "No #{path} here"}
   end
 
   def format_response(conv) do
   """
-  HTTP/1.1 200 OK
+  HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
   Content-Type: text/html
   Content-Length: #{String.length(conv.resp_body)}
 
   #{conv.resp_body}
   """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+      }[code]
   end
 end
 
@@ -57,7 +81,7 @@ IO.puts response
 
 
 request = """
-GET /bears HTTP/1.1
+GET /bears/1 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept:*/*
