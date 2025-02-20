f851520a3b757ea9dfd47ff46746eb39603dc732
loedn <loedn@pm.me>
2025-02-20 16:58:27 +0100

    cleaner now

--- lib/anton/handler.ex
+++ lib/anton/handler.ex
@@ -64,15 +64,20 @@ defmodule Anton.Handler do
     file =
       Path.expand("../../pages", __DIR__)
       |> Path.join("about.html")
+      |> File.read
+      |> handle_file(conv)
+  end
 
-    case File.read!(file) do
-      {:ok, content} ->
-        %{ conv | status: 200, resp_body: content}
-      {:error, :enoent} ->
-        %{ conv | status: 404, resp_body: "File not found"}
-      {:error, reason} ->
-        %{ conv | status: 500, resp_body: "File error: #{reason}"}
-    end
+  defp handle_file({:ok, content}, conv) do
+    %{ conv | status: 200, resp_body: content}
+  end
+
+  defp handle_file({:error, :enoent}, conv) do
+    %{ conv | status: 404, resp_body: "File not found"}
+  end
+
+  defp handle_file({:error, reason}, conv) do
+    %{ conv | status: 500, resp_body: "File error: #{reason}"}
   end
 
   # catchall function needs to be last
